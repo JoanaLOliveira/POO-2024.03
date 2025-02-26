29b1748b16b2093eaf08f1f836ba3356d819f92d
JoanaLOliveira <joanalmarcolino@gmail.com>
2025-02-26 15:27:22 -0300

    aula

---
+++ listaexe/ArrayEArraylist/Main.java
@@ -0,0 +1,11 @@
+package listaexe.ArrayEArraylist;
+
+public class Main {
+    public static void main(String[] args) {
+        int dados[][]=new int [5][];
+        for(int i = 0; i<5; i++){
+            dados[i][j]=i+j;
+            System.out.println(dados[i][j]);
+        }
+    }
+}
---
+++ listaexe/classep1/Time.java
@@ -0,0 +1,9 @@
+/* Escreva uma classe para representar um time de um esporte qualquer em um campeonato desse esporte. Que atributos devem ser 
+representados nessa classe? Quais métodos ela deve conter? Escreva um aplicativo de teste que demonstre as capacidades da
+classe criada. */
+
+package listaexe.classep1;
+
+public class Time {
+    
+}
